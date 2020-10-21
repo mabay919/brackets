5419fa04ae09fd30f88713122d6096675ed5fc7f
mabay919 <54597981+mabay919@users.noreply.github.com>
2020-10-21 19:25:55 +0300

    doc: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+let key = bracketsConfig.map(item => item.join(""));
+for (let i = 0; i < key.length; i += 1) {
+  while (str.includes(key[i])) {
+    str = str.replace(key[i], "");
+    i = 0;
+  }
+ 
+}
+return str.length === 0;
 }
